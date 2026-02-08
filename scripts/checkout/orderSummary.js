625c427f28e3230c21882cd1bfc1a9f7eade7409
youngcoderplus <johngithinji.jgm910@gmail.com>
2026-02-08 10:36:38 +0300

    Create automated test for order summary

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -1,8 +1,8 @@
 import { cart, removeFromCart, updateDeliveryOption } from "../../data/cart.js";    
 import { products, getProduct } from "../../data/products.js";
 import { formatCurrency } from "../utils/money.js";
-import { hello } from " https://unpkg.com/supersimpledev@1.0.1/hello.esm.js";
-import dayjs from ' https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
+import { hello } from "https://unpkg.com/supersimpledev@1.0.1/hello.esm.js";
+import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
 import { deliveryOptions, getDeliveryOption} from "../../data/deliveryOptions.js";
 import { renderPaymentSummary } from "./paymentSummary.js"; 
 
@@ -50,6 +50,7 @@ export function renderOrderSummary(){
 
     `
     <div class="cart-item-container 
+        js-cart-item-container
         js-cart-item-container-${matchingProduct.id}">
         <!-- Delivery date hidden 
             <div class="delivery-date">
@@ -68,14 +69,17 @@ export function renderOrderSummary(){
             <div class="product-price">
             ${formatCurrency(matchingProduct.priceCents)}
             </div>
-            <div class="product-quantity">
+            <div class="product-quantity
+            js-product-quantity-${matchingProduct.id}">
                 <span>
                 Quantity: <span class="quantity-label">${cartItem.quantity}</span>
                 </span>
                 <span class="update-quantity-link link-primary">
                 Update
                 </span>
-                <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
+                <span class="delete-quantity-link link-primary js-delete-link
+                js-delete-link-${matchingProduct.id}" 
+                data-product-id="${matchingProduct.id}">
                 Delete
                 </span>
             </div>
