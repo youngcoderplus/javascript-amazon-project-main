f45b4f7aa45f427094c3f6c1128149379b7d1b6f
youngcoderplus <johngithinji.jgm910@gmail.com>
2026-02-23 10:14:46 +0300

    Update again

--- tests/checkout/orderSummaryTest.js
+++ tests/checkout/orderSummaryTest.js
@@ -7,8 +7,8 @@ describe('test suite: renderOrderSummary', () => {
     const productId1 = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6';
     const productId2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d';
 
-    beforeAll((done) => {//the product has not come yet so we need to wait for response
-        loadProducts(() => {
+    beforeAll((done) => {
+        loadProducts(() => {//the product has not come yet so we need to wait for response
                     done();//if we dont't call done then beforeAll will wait forever
         });
     });
